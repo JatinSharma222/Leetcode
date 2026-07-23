1e32596b7717149083f66c94b10172d5d857d18f
JatinSharma222 <sharma.jatin2206@gmail.com>
2026-07-23 19:41:53 +0530

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+// @ts-ignore: side-effect import of CSS without type declaration
+import "./index.css";
+
+export function App() {
+  return (
+    <div className="app">
+      LeetCode
+    </div>
+  );
+}
+
+export default App;
